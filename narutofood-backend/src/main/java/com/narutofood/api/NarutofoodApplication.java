ee376e7a5486a2544814b02cd0d684ad96424985
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 22:26:13 -0300

    Projeto criado

---
+++ narutofood-backend/src/main/java/com/narutofood/api/NarutofoodApplication.java
@@ -0,0 +1,13 @@
+package com.narutofood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutofoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutofoodApplication.class, args);
+	}
+
+}
---
+++ narutofood-backend/src/test/java/com/narutofood/api/NarutofoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narutofood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutofoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
