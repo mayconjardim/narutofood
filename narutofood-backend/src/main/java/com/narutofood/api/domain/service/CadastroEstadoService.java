88ec6ed69991a7a2842e402f35420c77a9f021c5
Maycon Jardim <mayconsmill@gmail.com>
2023-03-02 17:59:24 -0300

    CadastroEstadoService

---
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/service/CadastroEstadoService.java
@@ -0,0 +1,34 @@
+package com.narutofood.api.domain.service;
+
+import com.narutofood.api.domain.exception.EntidadeEmUsoException;
+import com.narutofood.api.domain.exception.EntidadeNãoEncontradaException;
+import com.narutofood.api.domain.model.Estado;
+import com.narutofood.api.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Estado save(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    public void delete(Long id) {
+        try {
+            estadoRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNãoEncontradaException(String.format("Não existe uma estado com código %d", id));
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format("Estado de código %d não pode ser removida, pois está em uso", id));
+        }
+    }
+
+}
