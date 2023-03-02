6632a585e8414cb3bf2b8b5f542dc3dab06b71e5
Maycon Jardim <mayconsmill@gmail.com>
2023-03-02 13:52:25 -0300

    Estado entidade

---
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/model/Estado.java
@@ -0,0 +1,23 @@
+package com.narutofood.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class Estado implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+}
