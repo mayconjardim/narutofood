edadeb1d87473183a4cab0c36228ca9799baa423
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 23:13:00 -0300

    Restaurante entidade

--- narutofood-backend/src/main/java/com/narutofood/api/domain/model/Restaurante.java
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/model/Restaurante.java
@@ -1,25 +1,25 @@
 package com.narutofood.api.domain.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @Entity
-public class Cozinha implements Serializable {
+public class Restaurante implements Serializable {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private BigDecimal taxaFrete;
 
 }
