6e792aabfaddd262ea504f7c46ab2197c17ac7bb
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 23:27:00 -0300

    Relacionamento Muitos para um. Restaurante para Cozinha

--- narutofood-backend/src/main/java/com/narutofood/api/domain/model/Restaurante.java
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/model/Restaurante.java
@@ -3,10 +3,7 @@ package com.narutofood.api.domain.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -21,5 +18,8 @@ public class Restaurante implements Serializable {
     private Long id;
     private String nome;
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
 
 }
--- narutofood-backend/src/main/resources/import.sql
+++ narutofood-backend/src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into cozinha (nome) values ('Japonesa');
-insert into cozinha (nome) values ('Indiana');
+insert into cozinha (nome) values ('Francesa');
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values  ('Shoyu & Wasab', 9.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values  ('Amour Bistrô', 10, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values  ('Petit Rio', 9.50, 2);
