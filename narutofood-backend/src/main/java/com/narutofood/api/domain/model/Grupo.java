89b43face637025159f01698132d22bfc8d74b46
Maycon Jardim <mayconsmill@gmail.com>
2023-03-04 21:43:32 -0300

    Associação muitos para muitos entre Grupo e Permissao

--- narutofood-backend/src/main/java/com/narutofood/api/domain/model/Grupo.java
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/model/Grupo.java
@@ -1,22 +1,29 @@
 package com.narutofood.api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @Entity
-public class Grupo {
+public class Grupo implements Serializable {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
 
 }
