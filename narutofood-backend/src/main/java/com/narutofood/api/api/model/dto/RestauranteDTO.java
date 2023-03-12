d12a3ffb42a484aafc51e7631497274b1b3ece6a
Maycon Jardim <mayconsmill@gmail.com>
2023-03-12 13:12:40 -0300

    Criando ResturanteDTO

---
+++ narutofood-backend/src/main/java/com/narutofood/api/api/model/dto/RestauranteDTO.java
@@ -0,0 +1,21 @@
+package com.narutofood.api.api.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class RestauranteDTO {
+
+    private Long  id;
+
+    private String nome;
+
+    private BigDecimal taxaFrete;
+
+    private CozinhaDTO cozinha;
+
+
+}
