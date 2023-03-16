c9041712f387d992841cba7e7ce3d581bd0b3d60
Maycon Jardim <mayconsmill@gmail.com>
2023-03-16 14:59:52 -0300

    Permissao Repository e Service

--- narutofood-backend/src/main/java/com/narutofood/api/domain/model/Grupo.java
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/model/Grupo.java
@@ -7,7 +7,9 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
@@ -24,6 +26,14 @@ public class Grupo implements Serializable {
     @JoinTable(name = "grupo_permissao",
             joinColumns = @JoinColumn(name = "grupo_id"),
             inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-    private List<Permissao> permissoes = new ArrayList<>();
+    private Set<Permissao> permissoes = new HashSet<>();
+
+    public boolean removePermissao(Permissao permissao) {
+        return getPermissoes().remove(permissao);
+    }
+
+    public boolean addPermissao(Permissao permissao) {
+        return getPermissoes().add(permissao);
+    }
 
 }
