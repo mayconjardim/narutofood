1b0ec8f313c3513bd37b58cc6f79bc0391925663
Maycon Jardim <mayconsmill@gmail.com>
2023-03-16 12:48:32 -0300

    GrupoRepositor e CadastroGrupoService

---
+++ narutofood-backend/src/main/java/com/narutofood/api/domain/repository/GrupoRepository.java
@@ -0,0 +1,8 @@
+package com.narutofood.api.domain.repository;
+
+import com.narutofood.api.domain.model.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+
+}
