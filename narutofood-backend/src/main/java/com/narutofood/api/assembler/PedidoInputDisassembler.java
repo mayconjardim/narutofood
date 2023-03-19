402e8d1b1a199af95654b2095a06f80da669b435
Maycon Jardim <mayconsmill@gmail.com>
2023-03-19 13:12:29 -0300

    PedidoInputDisassembler

---
+++ narutofood-backend/src/main/java/com/narutofood/api/assembler/PedidoInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.narutofood.api.assembler;
+
+import com.narutofood.api.model.input.PedidoInput;
+import com.narutofood.domain.model.Pedido;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Pedido toDomainObject(PedidoInput pedidoInput) {
+        return modelMapper.map(pedidoInput, Pedido.class);
+    }
+
+    public void copyToDomainObject(PedidoInput pedidoInput, Pedido pedido) {
+        modelMapper.map(pedidoInput, pedido);
+    }
+}
